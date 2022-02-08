f9c4dc44ff02afbcd934114dfa08c1f164eb0235
dwaggoner24 <91644175+dwaggoner24@users.noreply.github.com>
2022-02-08 14:54:47 -0600

    Added routes

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
-const { Book } = require('../models');
+const {Book} = require('../models');
+const book = require('../models/book');
 
-//asyncHandler
+//handler function to wrap each route
 function asyncHandler(cb){
   return async(req,res,next) => {
     try {
       await cb(req,res,next)
     } catch(error) {
+      res.status(500).send(error);
       next(error);
     }
   }
@@ -15,13 +17,82 @@ function asyncHandler(cb){
 /* GET home page. */
 //Books Route
 //home route
-
 router.get('/', asyncHandler(async (req, res) => {
-  const books = await Book.findAll();
-  res.json(books);
+  res.redirect('/books');
   }));
 
+//shows full list of books
+router.get('/books', asyncHandler(async (req, res) => {
+  const books = await Book.findAll();
+  if (books) {
+    res.render('index', {books})
+  } else {
+    res.status(404).render('error');
+  }
+  }));
+
+//shows create new book form
+router.get('/books/new', asyncHandler(async (req, res) => {
+    res.render('new-book', {book: {}, title: 'New Book'});
+  }));
+
+//posts a new book to database
+router.post('/books/new', asyncHandler(async (req, res) => {
+  let book;
+  try {
+    const book = await Book.create(req.body);
+    res.redirect('/') //redirects to the home route
+  } catch (error) {
+    if(error.name === "SequelizeValidationError") { //checking the error
+      book = await Book.build(req.body);
+      res.render('new-book', {book, errors: error.errors, title: 'New Book'})
+    } else {
+      throw error; //error caught in the asyncHandler's catch block
+    }
+
+  }
+  
+}));
+
+//shows book details form
+router.get('/books/:id', asyncHandler(async (req, res) => {
+    const book = await Book.findByPk(req.params.id);
+    res.render('update-book', {book, title: book.title});
+    }));
+
+//updates book info in database
+router.post('/books/:id', asyncHandler(async (req, res) => {
+    let book;
+    try {
+      await Book.findByPk(req.params.id);
+      if (book) {
+        await book.update(req.body);
+        res.redirect('/');
+      } else {
+        res.sendStatus(404);
+      }
+    } catch (error) {
+      if(error.name === "SequelizeValidationError") { //checking the error
+        book = await Book.build(req.body);
+        res.render('update-book', {book, errors: error.errors, title: 'New Book'})
+      } else {
+        throw error; //error caught in the asyncHandler's catch block
+      }
+    }
+    
+}));
+
+// //deletes book -- PERMANENT 
+router.post('/books/:id/delete', asyncHandler(async (req, res) => {
+    const book = await Book.findByPk(req.params.id);
+    if (book) {
+      await book.destroy();
+      res.redirect('/');
+    } else {
+      res.sendStatus(404);
+    }
+}));
 
 
 
-module.exports = router;
+ module.exports = router;
